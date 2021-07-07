1e07665e6d0a77d3097ea95cf9e814abe994c0ce
kanghanhee <kanghanhee59@gmail.com>
2021-07-07 23:26:50 +0900

    Feat: 출국 공항별 대상견 찾기 API 구현

--- src/api/dogs.ts
+++ src/api/dogs.ts
@@ -10,12 +10,28 @@ const router = Router();
  */
 router.get("/", async (req: Request, res: Response) => {
   try {
-    const dogs = await Dog.find({status:'wating'}).sort('registerDate');
+    const dogs = await Dog.find({ status: 'wating' }).sort('registerDate');
     const totalNum = dogs.length;
 
     const response = { data: dogs, totalNum: totalNum };
-
+    
     res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ *  @route GET api/dogs/search/:endingAirport
+ *  @desc Get all dogs filterd by airport (오래된순)
+ *  @access Public
+ */
+ router.get("/search/:endingAirport", async (req: Request, res: Response) => {
+  try {
+    const searchedDog = await Dog.find({ endingAirport: req.params.endingAirport, status: 'wating' }).sort('registerDate');
+
+    res.json(searchedDog);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
