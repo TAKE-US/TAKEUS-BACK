26841c98d2b1b4fd6136266602841643a974e3be
qkrwjdan <madogisa12@naver.com>
2021-07-08 00:05:03 +0900

    Style: 코드 스타일 수정

--- src/api/dogs.ts
+++ src/api/dogs.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
 import multer from "multer";
 
-import { imageFilter, cleanFolder } from '../utils/filter';
+import { imageFilter, cleanFolder } from "../utils/filter";
 import Dog from "../models/Dog";
 
 const router = Router();
-const UPLOAD_PATH = 'uploads';
+const UPLOAD_PATH = "uploads";
 const upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: imageFilter });
 
 /**
@@ -15,12 +15,11 @@ const upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: imageFilter });
  */
 router.get("/", async (req: Request, res: Response) => {
   try {
-    const dogs = await Dog.find().sort('registerDate');
+    const dogs = await Dog.find().sort("registerDate");
     const totalNum = dogs.length;
 
     const response = { data: dogs, totalNum: totalNum };
     res.json(response);
-
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
@@ -32,16 +31,16 @@ router.get("/", async (req: Request, res: Response) => {
  *  @desc Get one dog with matching id.
  *  @access Public
  */
- router.get("/detail/:dogId", async (req: Request, res: Response) => {
+router.get("/detail/:dogId", async (req: Request, res: Response) => {
   try {
     const dogId = req.params.dogId;
-    const dog = await Dog.findOne({_id: dogId});
+    const dog = await Dog.findOne({ _id: dogId });
 
-    if (!dog) return res.status(400).json({ status: 400, msg: "Dog not found" });
+    if (!dog)
+      return res.status(400).json({ status: 400, msg: "Dog not found" });
 
     const response = { data: dog };
     res.status(200).json(response);
-
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
@@ -53,15 +52,14 @@ router.get("/", async (req: Request, res: Response) => {
  *  @desc Create one dog
  *  @access Public
  */
-router.post('/', upload.array("photos",5), async (req, res) => {
+router.post("/", upload.array("photos", 5), async (req, res) => {
   try {
     console.log(req.body);
     console.log(req.files);
     res.json("Hello");
   } catch (err) {
-      res.sendStatus(400);
+    res.sendStatus(400);
   }
-})
-
+});
 
 module.exports = router;
--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,16 +1,16 @@
-import * as del from 'del';
+import * as del from "del";
 
 const imageFilter = function (req, file, cb) {
-    // accept image only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
+  // accept image only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
 };
 
 const cleanFolder = function (folderPath) {
-    // delete files inside folder but not the folder itself
-    del.sync([`${folderPath}/**`, `!${folderPath}`]);
+  // delete files inside folder but not the folder itself
+  del.sync([`${folderPath}/**`, `!${folderPath}`]);
 };
 
-export { imageFilter, cleanFolder }
+export { imageFilter, cleanFolder };
