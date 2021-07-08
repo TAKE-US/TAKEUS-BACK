4aafd289aa160dd378def95468625c05444977ea
qkrwjdan <madogisa12@naver.com>
2021-07-08 12:26:14 +0900

    Chore: 복구시도 2

--- src/interfaces/IDog.ts
+++ src/interfaces/IDog.ts
@@ -13,7 +13,7 @@ export interface IDog {
   detail: string;
   user: mongoose.Types.ObjectId;
   isInstitution: boolean;
-  institutionName: string;
+  institution: string;
   status: string;
   kakaotalkId: string;
   phoneNumber: string;
@@ -21,4 +21,25 @@ export interface IDog {
   twitter: string;
   facebook: string;
   photos: Array<string>;
+}
+
+export interface IDogInputDTO {
+  name?: string;
+  gender?: string;
+  age?: number;
+  weight?: number;
+  health?: string;
+  neutralization?: boolean;
+  endingCountry?: string;
+  endingAirport?: string;
+  detail?: string;
+  user?: mongoose.Types.ObjectId;
+  isInstitution?: boolean;
+  institutionName?: string;
+  kakaotalkId?: string;
+  phoneNumber?: string;
+  instagram?: string;
+  twitter?: string;
+  facebook?: string;
+  photos?: Array<string>;
 }
