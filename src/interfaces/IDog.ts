bfec8a0981c2fddadc85e374c16c87f0b44909b4
qkrwjdan <madogisa12@naver.com>
2021-07-08 12:03:21 +0900

    Chore: 복구

--- src/interfaces/IDog.ts
+++ src/interfaces/IDog.ts
@@ -13,7 +13,7 @@ export interface IDog {
   detail: string;
   user: mongoose.Types.ObjectId;
   isInstitution: boolean;
-  institution: string;
+  institutionName: string;
   status: string;
   kakaotalkId: string;
   phoneNumber: string;
@@ -21,25 +21,4 @@ export interface IDog {
   twitter: string;
   facebook: string;
   photos: Array<string>;
-}
-
-export interface IDogInputDTO {
-  name?: string;
-  gender?: string;
-  age?: number;
-  weight?: number;
-  health?: string;
-  neutralization?: boolean;
-  endingCountry?: string;
-  endingAirport?: string;
-  detail?: string;
-  user?: mongoose.Types.ObjectId;
-  isInstitution?: boolean;
-  institutionName?: string;
-  kakaotalkId?: string;
-  phoneNumber?: string;
-  instagram?: string;
-  twitter?: string;
-  facebook?: string;
-  photos?: Array<string>;
 }
