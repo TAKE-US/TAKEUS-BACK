d8b8d3633e839c2b7dff4205978d0b131f764725
qkrwjdan <madogisa12@naver.com>
2021-07-09 22:03:51 +0900

    Feat: 마이 페이지 게시물 가져오기, 특정 강아지 상태 수정, 특정 강아지 상태 전체 수정, 강아지 지우기

--- src/api/dogs.ts
+++ src/api/dogs.ts
@@ -56,7 +56,7 @@ router.get("/", async (req: Request, res: Response) => {
 router.get("/detail/:dogId", async (req: Request, res: Response) => {
   try {
     const dogId = req.params.dogId;
-    const dog = await Dog.findOne({ _id: dogId });
+    const dog = await Dog.findOne({ _id: dogId, status: { $ne: "deleted" } });
 
     if (!dog)
       return res.status(400).json({ status: 400, msg: "Dog not found" });
@@ -172,5 +172,192 @@ router.post(
     }
   }
 );
+
+/**
+ *  @route GET api/dogs/my
+ *  @desc Get my dogs
+ *  @access Private
+ */
+router.get("/my", auth, async (req: Request, res: Response) => {
+  try {
+    const userId = req.body.user.id;
+    const dogs = await Dog.find({
+      user: userId,
+      status: { $ne: "deleted" },
+    }).sort({ status: -1, registerDate: -1 });
+    const totalNum = dogs.length;
+    // if (!dog)
+    //   return res.status(400).json({ status: 400, msg: "Dog not found" });
+
+    const response = { data: dogs, totalNum: totalNum };
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ *  @route PUT api/dogs/detail/:dogId
+ *  @desc Update dog
+ *  @access Private
+ */
+ router.put(
+  "/detail/:dogId",
+  upload.array("photos", 5),
+  auth,
+  aws.imageUploadToS3,
+  async (req, res) => {
+
+    const userId = req.body.user.id;
+    const dogId = req.params.dogId;
+
+    let dog = await Dog.findOne({ _id: dogId, status: { $ne: "deleted" } });
+
+    if (!dog)
+      return res.status(400).json({ status: 400, msg: "Dog not found" });
+
+    const owner = dog.user;
+
+    if (userId != owner) {
+      res.status(403).json({ msg: "Invalid access. no authenticated." });
+    }
+
+    const {
+      endingCountry,
+      endingAirport,
+      name,
+      gender,
+      age,
+      weight,
+      neutralization,
+      health,
+      isInstitution,
+      institutionName,
+      kakaotalkId,
+      phoneNumber,
+      instagram,
+      twitter,
+      facebook,
+      detail,
+      photos
+    } = req.body;
+
+    if (endingCountry) dog.endingCountry = endingCountry;
+    if (endingAirport) dog.endingAirport = endingAirport;
+    if (name) dog.name = name;
+    if (gender) dog.gender = gender;
+    if (age) dog.age = age;
+    if (weight) dog.weight = weight;
+    if (neutralization) dog.neutralization = neutralization;
+    if (health) dog.health = health;
+    if (isInstitution) dog.isInstitution = isInstitution;
+    if (institutionName) dog.institutionName = institutionName;
+    if (kakaotalkId) dog.kakaotalkId = kakaotalkId;
+    if (phoneNumber) dog.phoneNumber = phoneNumber;
+    if (instagram) dog.instagram = instagram;
+    if (twitter) dog.twitter = twitter;
+    if (facebook) dog.facebook = facebook;
+    if (detail) dog.detail = detail;
+    if (photos) dog.photos = photos;
+
+    try {
+      // Update
+      await dog.save();
+
+      cleanFolder(`${UPLOAD_PATH}/`);
+
+      res.status(200).json(dog);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error.");
+    }
+  }
+);
+
+/**
+ *  @route PUT api/dogs/detail/:dogId/status
+ *  @desc Update dog's status
+ *  @access Private
+ */
+router.put(
+  "/detail/:dogId/status",
+  auth,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = req.body.user.id;
+      const status = req.body.status;
+      const dogId = req.params.dogId;
+
+      if (!(status == "waiting" || status == "done")) {
+        res.status(400).json({ msg: "유효하지 않은 값입니다." });
+      }
+
+      let dog = await Dog.findOne({ _id: dogId, status: { $ne: "deleted" } });
+      if (!dog)
+        return res.status(400).json({ status: 400, msg: "Dog not found" });
+
+      const owner = dog.user;
+
+      if (userId != owner) {
+        res.status(403).json({ msg: "Invalid access. no authenticated." });
+      }
+
+      dog.status = status;
+      dog.save();
+      const response = { data: dog };
+
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+/**
+ *  @route DELETE api/dogs/detail/:dogId/
+ *  @desc Delete dog
+ *  @access Private
+ */
+router.delete("/detail/:dogId", auth, async (req: Request, res: Response) => {
+  try {
+    const userId = req.body.user.id;
+    const dogId = req.params.dogId;
+
+    let dog = await Dog.findOne({ _id: dogId });
+    const owner = dog.user;
+
+    if (userId != owner) {
+      res.status(403).json({ msg: "Invalid access. no authenticated." });
+    }
+
+    dog.status = "deleted";
+    dog.save();
+    const response = { data: "deleted" };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ *  @route GET api/dogs/deleted
+ *  @desc Get deleted dogs
+ *  @access Public
+ */
+router.get("/deleted", async (req: Request, res: Response) => {
+  try {
+    const dogs = await Dog.find({ status: "deleted" });
+    const response = { data: dogs };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
