58f875c063b8b564a92811f5efee6bc0db84ae4c
kanghanhee <kanghanhee59@gmail.com>
2021-07-11 17:17:26 +0900

    Update: 공항&해시태그로 후기 조회

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -76,4 +76,38 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ *  @route GET api/reviews/:endingAirport
+ *  @desc Get all reviews filterd by endingAirport,hashtags
+ *  @access Public
+ */
+ router.get("/search/:endingAirport", async (req: Request, res: Response) => {
+  try {
+    const orderHash = { latest: -1, oldest: 1, undefined: -1 };
+
+    const order: any = req.query.order;
+    const hashtags: any = req.query.hashtags;
+    const { page = 1, postNumInPage = 7 } = req.query;
+
+    const { skip, limit } = calculateSKipAndLimit(
+      page as any as number,
+      postNumInPage as any as number
+    );
+
+    const reviews = await Review.find({
+      endingAirport: req.params.endingAirport,
+      hashtags: hashtags,
+    })
+      .sort({ writeDate: orderHash[order] })
+      .skip(skip)
+      .limit(limit);
+    const totalNum = await Review.countDocuments({});
+
+    res.status(200).json({ data: reviews, totalNum: totalNum });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
