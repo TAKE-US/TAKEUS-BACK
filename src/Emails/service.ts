d66cafc9fcc967869b74defa1d78425789982919
kanghanhee <kanghanhee59@gmail.com>
2021-10-08 19:24:08 +0900

    Fix: 문의하기 메일보내기 수정

--- src/Emails/service.ts
+++ src/Emails/service.ts
@@ -1,25 +1,24 @@
 import { SC } from "../utils/statusCode";
-import { RM } from "../utils/responseMessage";
-
 import config from "../config";
 
 const nodemailer = require("nodemailer");
 
 class EmailService {
-  async sendMail(name, email, text, user) {
+  async sendEmail(name, email, text) {
     let transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: 'gmail',
       auth: {
-        user: config.mailuser,
-        pass: config.mailpass,
-      },
+        user: config.mailuser,  // gmail 계정 아이디
+        pass: config.mailpass,  // gmail 계정의 비밀번호
+      }
     });
 
     let mailOptions = {
-      from: config.mailuser,
-      to: email,
-      subject: name,
-      text: text,
+      from: config.mailuser,              
+      to: config.mailuser,      // 수신 메일 주소
+      cc: email,                // 사용자 메일 주소
+      subject: name,            // 메일 제목
+      text: text,               // 메일 내용
     };
 
     transporter.sendMail(mailOptions);
