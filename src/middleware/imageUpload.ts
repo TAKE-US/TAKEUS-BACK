b8379fc71c4eff3bacc8cf0d05347eb6d41ab288
qkrwjdan <madogisa12@naver.com>
2021-10-12 17:18:06 +0900

    Feat: upload file 용량 제한

--- src/middleware/imageUpload.ts
+++ src/middleware/imageUpload.ts
@@ -4,6 +4,9 @@ import config from "../config";
 import multer from "multer";
 import { imageFilter } from "../utils/filter";
 
+import { SC } from "../utils/statusCode";
+import { RM } from "../utils/responseMessage";
+
 const URL = config.fileUploadServerUrl;
 
 // things for file upload.
@@ -11,6 +14,7 @@ const UPLOAD_PATH = "uploads";
 const upload = multer({
   dest: `${UPLOAD_PATH}/`,
   fileFilter: imageFilter,
+  limits: { fileSize: 1024 * 1024 * 15 },
 }).array("photos", 5);
 
 export default (req, res, next) => {
@@ -18,42 +22,45 @@ export default (req, res, next) => {
 
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
+      // file too large
+      res.status(SC.BAD_REQUEST).send({ error: err.message });
     } else if (err) {
-      // An unknown error occurred when uploading.
-    }
-    req.body.user = user;
+      // Only image files are allowed!
+      res.status(SC.BAD_REQUEST).send({ error: err.message });
+    } else {
+      req.body.user = user;
 
-    let photos = [];
+      let photos = [];
 
-    for (let file of req.files) {
-      photos.push({
-        value: fs.createReadStream(file.path),
-        options: {
-          filename: file.originalname,
-          contentType: file.mimetype,
-        },
-      });
-    }
-
-    const formData = {
-      photos: photos,
-    };
-
-    request.post(
-      { url: URL, formData: formData },
-      (err, httpResponse, body) => {
-        try {
-          const data = JSON.parse(body);
-          req.body.photos = data.data;
-          next();
-        } catch (error) {
-          console.error(error.message);
-          res
-            .status(500)
-            .send({ error: "Internal Error : fail to upload image!" });
-        }
+      for (let file of req.files) {
+        photos.push({
+          value: fs.createReadStream(file.path),
+          options: {
+            filename: file.originalname,
+            contentType: file.mimetype,
+          },
+        });
       }
-    );
+
+      const formData = {
+        photos: photos,
+      };
+
+      request.post(
+        { url: URL, formData: formData },
+        (err, httpResponse, body) => {
+          try {
+            const data = JSON.parse(body);
+            req.body.photos = data.data;
+            next();
+          } catch (error) {
+            console.error(error.message);
+            res
+              .status(500)
+              .send({ error: "Internal Error : fail to upload image!" });
+          }
+        }
+      );
+    }
   });
 };
