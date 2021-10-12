e1aee4db270438bed2999648a0793b50b0670e99
qkrwjdan <madogisa12@naver.com>
2021-10-12 16:54:31 +0900

    Fix: multer middlerware 함수로 변경하기

--- src/middleware/imageUpload.ts
+++ src/middleware/imageUpload.ts
@@ -1,34 +1,59 @@
 import fs from "fs";
 import request from "request";
 import config from "../config";
+import multer from "multer";
+import { imageFilter } from "../utils/filter";
 
 const URL = config.fileUploadServerUrl;
 
+// things for file upload.
+const UPLOAD_PATH = "uploads";
+const upload = multer({
+  dest: `${UPLOAD_PATH}/`,
+  fileFilter: imageFilter,
+}).array("photos", 5);
+
 export default (req, res, next) => {
-  let photos = [];
+  const user = req.body.user;
 
-  for (let file of req.files) {
-    photos.push({
-      value: fs.createReadStream(file.path),
-      options: {
-        filename: file.originalname,
-        contentType: file.mimetype,
-      },
-    });
-  }
-
-  const formData = {
-    photos: photos,
-  };
-
-  request.post({ url: URL, formData: formData }, (err, httpResponse, body) => {
-    try {
-      const data = JSON.parse(body);
-      req.body.photos = data.data;
-      next();
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send({ error: "Internal Error : fail to upload image!" });
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+    } else if (err) {
+      // An unknown error occurred when uploading.
     }
+    req.body.user = user;
+
+    let photos = [];
+
+    for (let file of req.files) {
+      photos.push({
+        value: fs.createReadStream(file.path),
+        options: {
+          filename: file.originalname,
+          contentType: file.mimetype,
+        },
+      });
+    }
+
+    const formData = {
+      photos: photos,
+    };
+
+    request.post(
+      { url: URL, formData: formData },
+      (err, httpResponse, body) => {
+        try {
+          const data = JSON.parse(body);
+          req.body.photos = data.data;
+          next();
+        } catch (error) {
+          console.error(error.message);
+          res
+            .status(500)
+            .send({ error: "Internal Error : fail to upload image!" });
+        }
+      }
+    );
   });
 };
