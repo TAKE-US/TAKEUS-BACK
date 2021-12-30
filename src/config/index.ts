fb32c10e61cbf20d9cf82bfb775bd6a5d2bdf319
qkrwjdan <madogisa12@naver.com>
2021-12-31 00:05:16 +0900

    Fix: Nodemon의 NODE_ENV 설정 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 export default {
   /**
